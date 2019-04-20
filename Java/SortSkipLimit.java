b6e21297451d2e870f69330f5bdf55e759c6c6a4
Teofilo Francisco <teopinillo@gmail.com>
2019-04-19 23:04:23 -0400

    Update SortSkipLimit.java

--- Java/SortSkipLimit.java
+++ Java/SortSkipLimit.java
@@ -1,15 +1,10 @@
- @Test
-  public void testSortSkipLimit() {
     /*
      * Getting all the cursor methods together is done by appending each of
      * those methods to the resulting find iterable
      */
     Bson sortBy_i_Descending = Sorts.descending("i");
-    Iterable<Document> cursor = sortable.find().sort(sortBy_i_Descending).skip(100).limit(10);
-    int iValue = 899;
-
-    for (Document d : cursor) {
-      Assert.assertEquals(Integer.valueOf(iValue), d.getInteger("i"));
-      iValue--;
-    }
-  }
+    Iterable<Document> cursor = sortable.find()
+                                        .sort(sortBy_i_Descending)
+                                        .skip(100)
+                                        .limit(10);
+  
